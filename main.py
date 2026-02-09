f6c43e1d30f3f0f319f0f9029f9d55fb1839f2fa
kmxsaran07-gif <kmxsaran07@gmail.com>
2026-02-09 18:10:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -876,21 +876,13 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
             elif "webvideos.classplusapp." in url:
                cmd = f'yt-dlp --add-header "referer:https://web.classplusapp.com/" --add-header "x-cdn-tag:empty" -f "{ytf}" "{url}" -o "{name}.mp4"'
-            elif "youtube.com" in url or "youtu.be" in url:
-    cmd = (
-        'yt-dlp '
-        '-o "%(id)s.%(ext)s" '
-        '--extractor-args "youtube:player_client=android" '
-        '--downloader ffmpeg '
-        '--hls-use-mpegts '
-        '--merge-output-format mp4 '
-        '--no-playlist '
-        f'"{url}"'
-    )
-
-
-
-
+            cmd = (
+    f'yt-dlp -o "{name}.mp4" "{url}" '
+    '--extractor-args "youtube:player_client=android" '
+    '--downloader ffmpeg '
+    '--merge-output-format mp4 '
+    '--no-playlist'
+)
 
             try:
                 cc = (
